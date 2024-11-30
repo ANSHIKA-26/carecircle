d666c9715163a1ca9a691dfba7856a01f4de6d27
ANSHIKA-26 <arora.anshika.26@gmail.com>
2024-12-01 00:29:04 +0530

    feat: Add MinimalNavbar component for registration/login pages
    - Refactor styles for improved layout

--- client/src/pages/ApplyDoctor.jsx
+++ client/src/pages/ApplyDoctor.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "../styles/register.css";
 import axios from "axios";
 import toast from "react-hot-toast";
+import MinimalNavbar from "../components/MinimalNavbar";
+
 
 function ApplyDoctor() {
   const [loading, setLoading] = useState(false);
@@ -69,84 +71,87 @@ function ApplyDoctor() {
   };
 
   return (
-    <section className="register-section flex-center">
-      <div className="register-container flex-center">
-        <h2 className="form-heading">Apply for Doctor</h2>
-        <form onSubmit={formSubmit} className="register-form">
-          <input
-            type="text"
-            name="firstname"
-            className="form-input"
-            placeholder="Enter your first name"
-            value={formDetails.firstname}
-            onChange={inputChange}
-          />
-          <input
-            type="text"
-            name="lastname"
-            className="form-input"
-            placeholder="Enter your last name"
-            value={formDetails.lastname}
-            onChange={inputChange}
-          />
-          <input
-            type="email"
-            name="email"
-            className="form-input"
-            placeholder="Enter your email"
-            value={formDetails.email}
-            onChange={inputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            className="form-input"
-            placeholder="Enter your password"
-            value={formDetails.password}
-            onChange={inputChange}
-          />
-          <input
-            type="password"
-            name="confpassword"
-            className="form-input"
-            placeholder="Confirm your password"
-            value={formDetails.confpassword}
-            onChange={inputChange}
-          />
-          <input
-            type="text"
-            name="specialization"
-            className="form-input"
-            placeholder="Enter your specialization"
-            value={formDetails.specialization}
-            onChange={inputChange}
-          />
-          <input
-            type="number"
-            name="experience"
-            className="form-input"
-            placeholder="Enter your experience in years"
-            value={formDetails.experience}
-            onChange={inputChange}
-          />
-          <input
-            type="number"
-            name="fees"
-            className="form-input"
-            placeholder="Enter your fees"
-            value={formDetails.fees}
-            onChange={inputChange}
-          />
-          <button
-            type="submit"
-            className="btn form-btn"
-            disabled={loading}
-          >
-            Apply
-          </button>
-        </form>
-      </div>
-    </section>
+    <>
+      <MinimalNavbar />
+      <section className="register-section flex-center">
+        <div className="register-container flex-center">
+          <h2 className="form-heading">Apply for Doctor</h2>
+          <form onSubmit={formSubmit} className="register-form">
+            <input
+              type="text"
+              name="firstname"
+              className="form-input"
+              placeholder="Enter your first name"
+              value={formDetails.firstname}
+              onChange={inputChange}
+            />
+            <input
+              type="text"
+              name="lastname"
+              className="form-input"
+              placeholder="Enter your last name"
+              value={formDetails.lastname}
+              onChange={inputChange}
+            />
+            <input
+              type="email"
+              name="email"
+              className="form-input"
+              placeholder="Enter your email"
+              value={formDetails.email}
+              onChange={inputChange}
+            />
+            <input
+              type="password"
+              name="password"
+              className="form-input"
+              placeholder="Enter your password"
+              value={formDetails.password}
+              onChange={inputChange}
+            />
+            <input
+              type="password"
+              name="confpassword"
+              className="form-input"
+              placeholder="Confirm your password"
+              value={formDetails.confpassword}
+              onChange={inputChange}
+            />
+            <input
+              type="text"
+              name="specialization"
+              className="form-input"
+              placeholder="Enter your specialization"
+              value={formDetails.specialization}
+              onChange={inputChange}
+            />
+            <input
+              type="number"
+              name="experience"
+              className="form-input"
+              placeholder="Enter your experience in years"
+              value={formDetails.experience}
+              onChange={inputChange}
+            />
+            <input
+              type="number"
+              name="fees"
+              className="form-input"
+              placeholder="Enter your fees"
+              value={formDetails.fees}
+              onChange={inputChange}
+            />
+            <button
+              type="submit"
+              className="btn form-btn"
+              disabled={loading}
+            >
+              Apply
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 }
 
