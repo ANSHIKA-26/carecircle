ee216c066ff0c34c581824a5f9307819fc4a995d
ANSHIKA_26 <arora.anshika.26@gmail.com>
2024-12-13 19:47:09 +0530

    corsfix

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ const notificationRouter = require("./routes/notificationRouter");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Hello endpoint
 app.get('/api/v1/hello', (req, res) => {
